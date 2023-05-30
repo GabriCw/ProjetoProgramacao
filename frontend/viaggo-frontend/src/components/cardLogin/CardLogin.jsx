7a9208f5e53cf051ce0742547833d44a9fe42939
Davi Soares <davi.soares@psatech.com.br>
2023-05-30 20:32:24 -0300

    css layout changes

--- frontend/viaggo-frontend/src/components/cardLogin/CardLogin.jsx
+++ frontend/viaggo-frontend/src/components/cardLogin/CardLogin.jsx
@@ -3,29 +3,33 @@ import logo from "../../assets/logo.png"
 import "./style.css"
 
 const CardLogin = () => {
-    return <div className="content">
-        <div className="card-content">
-            <div className="logo-content">
+    return <div className="container">
+        <div className="card-container">
+            <div className="logo-container">
                 <img src={logo} className="logo" />
                 <img src={barCode} className="barCode" />
             </div>
-            <div className="details-content">
+            <div className="details-container">
                 <div className="hole1" />
                 <div className="line" />
                 <div className="hole2" />
             </div>
-            <div className="data-content">
-                <h1 className="welcome-title">BEM VINDO PASSAGEIRO</h1>
-                <h2 className="title">FAÇA SEU LOGIN</h2>
-                <div className="input-content">
-                    <input className="input" placeholder="CPF ou E-MAIL" />
-                    <input className="input" placeholder="SENHA" />
+            <div className="data-container">
+                <div className="titles-container">
+                    <h1 className="welcome-title">BEM VINDO PASSAGEIRO</h1>
+                    <h2 className="title">FAÇA SEU LOGIN</h2>
                 </div>
-                <div className="others-content">
-                    <p className="others-options">ESQUECI MINHA SENHA</p>
-                    <p className="others-options">CRIAR CONTA</p>
+                <div className="login-container">
+                    <div className="input-container">
+                        <input className="input" type="email" placeholder="CPF ou E-MAIL" />
+                        <input className="input" type="password" placeholder="SENHA" />
+                    </div>
+                    <div className="others-container">
+                        <p className="others-options">ESQUECI MINHA SENHA</p>
+                        <p className="others-options">CRIAR CONTA</p>
+                    </div>
                 </div>
-                <div className="button-content">
+                <div className="button-container">
                     <button className="button-login">ENTRAR</button>
                 </div>
             </div>
