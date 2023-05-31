5c30bf414140fd0de35301ba1549a93141f1d7b7
Gabriel <gabricouto2305@gmail.com>
2023-05-30 23:59:01 -0300

    Semantica Feita

--- frontend/viaggo-frontend/src/components/cardLogin/CardLogin.jsx
+++ frontend/viaggo-frontend/src/components/cardLogin/CardLogin.jsx
@@ -5,34 +5,36 @@ import "./style.css"
 const CardLogin = () => {
     return <div className="container">
         <div className="card-container">
-            <div className="logo-container">
-                <img src={logo} className="logo" />
-                <img src={barCode} className="barCode" />
-            </div>
+            <figure className="logo-container">
+                <img src={logo} className="logo" alt="Logo ViagGo" />
+                <img src={barCode} className="barCode" alt="Código de barras genérico"/>
+            </figure>
             <div className="details-container">
                 <div className="hole1" />
                 <div className="line" />
                 <div className="hole2" />
             </div>
-            <div className="data-container">
-                <div className="titles-container">
+            <main className="data-container">
+                <header className="titles-container">
                     <h1 className="welcome-title">BEM VINDO PASSAGEIRO</h1>
                     <h2 className="title">FAÇA SEU LOGIN</h2>
-                </div>
-                <div className="login-container">
-                    <div className="input-container">
-                        <input className="input" type="email" placeholder="CPF ou E-MAIL" />
-                        <input className="input" type="password" placeholder="SENHA" />
+                </header>
+                <form>
+                    <section className="login-container">
+                        <div className="input-container">
+                            <input className="input" type="email" placeholder="CPF ou E-MAIL" />
+                            <input className="input" type="password" placeholder="SENHA" />
+                        </div>
+                        <div className="others-container">
+                            <p className="others-options">ESQUECI MINHA SENHA</p>
+                            <p className="others-options">CRIAR CONTA</p>
+                        </div>
+                    </section>
+                    <div className="button-container">
+                        <button className="button-login">ENTRAR</button>
                     </div>
-                    <div className="others-container">
-                        <p className="others-options">ESQUECI MINHA SENHA</p>
-                        <p className="others-options">CRIAR CONTA</p>
-                    </div>
-                </div>
-                <div className="button-container">
-                    <button className="button-login">ENTRAR</button>
-                </div>
-            </div>
+                </form>
+            </main>
         </div>
 
     </div>
