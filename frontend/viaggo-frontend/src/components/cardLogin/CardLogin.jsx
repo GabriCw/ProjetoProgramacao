60d5d6d037ab6dd8dcef204ca7e377b7d0525c50
Davi Soares <davi.soares@psatech.com.br>
2023-06-01 01:38:53 -0300

    eye password and css alterations

--- frontend/viaggo-frontend/src/components/cardLogin/CardLogin.jsx
+++ frontend/viaggo-frontend/src/components/cardLogin/CardLogin.jsx
@@ -1,16 +1,32 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import barCode from "../../assets/barCode.png"
 import logo from "../../assets/logo.png"
 import "./style.css"
-import { useNavigate } from "react-router-dom"
 
 const CardLogin = () => {
     const navigate = useNavigate()
+
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [typePasswordInput, setTypePasswordInput] = useState("password");
+
+    const handlePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    }
+
+    useEffect(() => {
+        if (isPasswordVisible)
+            setTypePasswordInput("text");
+        else
+            setTypePasswordInput("password");
+    }, [isPasswordVisible])
 
     return <div className="container">
         <div className="card-container">
             <figure className="logo-container">
                 <img src={logo} className="logo" alt="Logo ViagGo" />
-                <img src={barCode} className="barCode" alt="Código de barras genérico"/>
+                <img src={barCode} className="barCode" alt="Código de barras genérico" />
             </figure>
             <div className="details-container">
                 <div className="hole1" />
@@ -26,7 +42,17 @@ const CardLogin = () => {
                     <section className="login-container">
                         <div className="input-container">
                             <input className="input" type="email" placeholder="CPF ou E-MAIL" />
-                            <input className="input" type="password" placeholder="SENHA" />
+                            <div className="password-input-container">
+                                <input className="input" type={typePasswordInput} placeholder="SENHA" />
+                                <div onClick={handlePasswordVisibility}>
+                                    {
+                                        isPasswordVisible ?
+                                            <Visibility className="icon" />
+                                            :
+                                            <VisibilityOff className="icon" />
+                                    }
+                                </div>
+                            </div>
                         </div>
                         <div className="others-container">
                             <button onClick={() => navigate('/escsenha')} className="others-options">ESQUECI MINHA SENHA</button>
--- frontend/viaggo-frontend/src/index.js
+++ frontend/viaggo-frontend/src/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Login from './views/login/Login';
 import Cadastro from './views/cadastro/Cadastro';
 import EscSenha from './views/escSenha/EscSenha';
-
-import{ BrowserRouter, Routes, Route } from "react-router-dom"
-
+import Login from './views/login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route index element={<Login/>} />
-    <Route path="/login" element={<Login/>} />
-    <Route path="/cadastro" element={<Cadastro/>} />
-    <Route path="/escsenha" element={<EscSenha/>} />
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/escsenha" element={<EscSenha />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
